41e3af77580aa4c7674e1a9a3cc2b973d4ea5454
mafdeashak-dot <mafdeashak@gmail.com>
2025-11-24 01:07:06 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,13 +1,4 @@
 
-window.addEventListener("scroll", function(){
-    const header = document.querySelector("header");
-    if(window.scrollY > 50){
-        header.classList.add("scrolled");
-    } else {
-        header.classList.remove("scrolled");
-    }
-});
-
 
 
 const menu = document.querySelector("nav ul");
@@ -16,3 +7,4 @@ const toggle = document.querySelector(".menu-toggle");
 toggle.addEventListener("click", () => {
     menu.classList.toggle("show");
 });
+
